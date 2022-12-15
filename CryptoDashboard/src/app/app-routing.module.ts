c60d72f4307e8359723fa57f802ee2b819a72149
sol.D.ace <loic.sorroche@isen.yncrea.fr>
2022-12-15 10:50:40 +0100

    fin du link main et about, plus debut page about

--- CryptoDashboard/src/app/app-routing.module.ts
+++ CryptoDashboard/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
