2b1d14d9ff8a5aea0d81f20813c8bf7618e3e01a
Hugo-Azix <hugo.millet@isen.yncrea.fr>
2022-12-26 09:25:58 -0600

    Recupération API CHART

--- CryptoDashboard/src/app/coinsinfo/coinsinfo.component.ts
+++ CryptoDashboard/src/app/coinsinfo/coinsinfo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SingleCryptoInfoService } from '../single-crypto-info.service';
+import { SingleCryptoGraphService } from '../single-crypto-graph.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -11,8 +12,9 @@ export class CoinsinfoComponent implements OnInit {
   id: string
   sub: any
   crypto : any
+  chartInfoTabPrices : Array<any> = new Array<any>()
 
-  constructor(private SingleCryptoInfoService: SingleCryptoInfoService,private route: ActivatedRoute) {
+  constructor(private SingleCryptoInfoService: SingleCryptoInfoService,private SingleCryptoGraphService: SingleCryptoGraphService,private route: ActivatedRoute) {
     this.id = "none"
    }
 
@@ -27,5 +29,26 @@ export class CoinsinfoComponent implements OnInit {
         this.crypto = data
       }
     )
+
+    this.SingleCryptoGraphService.HistoricalChart(this.id,"usd",30).subscribe(
+      (data) => {
+        console.log(data)
+        this.chartInfoTabPrices = data.prices
+      }
+    );
+
+
+
   }
+
+
+  // get date from chartInfo and return it in a string format for the chart to display it correctly
+  getDate = (date: number) : string => {
+    let d = new Date(date)
+    let day = d.getDate()
+    let month = d.getMonth() + 1
+    let year = d.getFullYear()
+    return day + "/" + month + "/" + year
+  }
+
 }
