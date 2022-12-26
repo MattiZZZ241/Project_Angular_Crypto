5efd38a5a656b57b5c183f51215fdb8096cfbe75
Matthis BERNET <matthis2401@gmail.com>
2022-12-26 14:38:42 +0100

    Refactor des components + tab Material
    main -> crypto-all
    all-crypto-list -> main
    crypto -> x

--- CryptoDashboard/src/app/app-routing.module.ts
+++ CryptoDashboard/src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { CoinsinfoComponent } from './coinsinfo/coinsinfo.component';
-import { MainComponent } from './main/main.component';
+import { CryptoAllComponent } from './crypto-all/crypto-all.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: MainComponent },
+  { path: 'home', component: CryptoAllComponent },
   { path: 'about', component: AboutComponent },
   { path: 'coins/:id', component: CoinsinfoComponent}
 ];
